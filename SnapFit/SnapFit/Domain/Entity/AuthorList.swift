153f7b52a23f381f553c92f8f1aa02469f4c5dcf
jsa0224 <voidness45@gmail.com>
2024-11-22 22:13:56 +0900

    feat: Domain - Entity 파트 추가

---
+++ SnapFit/SnapFit/Domain/Entity/AuthorList.swift
@@ -0,0 +1,26 @@
+//
+//  AuthorList.swift
+//  SnapFit
+//
+//  Created by 정선아 on 11/22/24.
+//
+
+
+//
+//  AuthorListModel.swift
+//  SnapFit
+//
+//  Created by 정정욱 on 8/15/24.
+//  
+//
+import Foundation
+
+enum AuthorList {
+    enum LoadAuthorList {
+        struct Request {}
+        
+        struct Response {}
+        
+        struct ViewModel {}
+    }
+}
---
+++ SnapFit/SnapFit/Domain/Entity/MyPage.swift
@@ -0,0 +1,28 @@
+//
+//  MyPage.swift
+//  SnapFit
+//
+//  Created by 정선아 on 11/22/24.
+//
+
+
+//
+//  MyPageModel.swift
+//  SnapFit
+//
+//  Created by 정정욱 on 8/12/24.
+//  
+//
+import Foundation
+
+enum MyPage {
+    enum LoadMyPage {
+        struct Request {}
+        
+        struct Response {}
+        
+        struct ViewModel {
+            var logOut : Bool = false
+        }
+    }
+}
